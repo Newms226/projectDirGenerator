d4cc99b381b462b21dcbdb99fe00f599e7e8e82a
Michael Newman <newms226@gmail.com>
2018-10-03 17:05:31 -0600

    Version 0.1: All files added

--- fileTools/filetools.py
+++ fileTools/filetools.py
@@ -1,25 +1,36 @@
+import os
 from pathlib import Path
 
-import datetime, subprocess
+import datetime
 
 def makeProjDir(rootDir: str, projectName: str, remotePath: str):
-    init_git_repo(rootDir, projectName)
-
     projDirStr = rootDir + "/" + projectName
+    init_git_repo(rootDir, projDirStr)
+
     Path(projDirStr + '/' + 'docs').mkdir()
     Path(projDirStr + '/' + 'test').mkdir()
     Path(projDirStr + '/' + 'builds').mkdir()
+    Path(projDirStr + '/' + 'env').mkdir()
+    Path(projDirStr + '/' + 'resources').mkdir()
     Path(projDirStr + '/' + projectName + '_src').mkdir()
-    generateBasicTextFile(projDirStr, 'readme')
-    generateBasicTextFile(projDirStr, 'license')
-    generateBasicTextFile(projDirStr, 'contributors', 'Michael Newman')
-    generateBasicTextFile(projDirStr, 'versions', 'txt',
-                          'v0.1.0 Generated on ' + datetime.datetime.now() +
-                          '\n')
+    generateBasicTextFile(projDirStr, 'readme', 'rst', "")
+    generateBasicTextFile(projDirStr, 'license', 'rst', "")
+    generateBasicTextFile(projDirStr, 'contributors', 'rst',
+                          '  - Michael Newman')
+    generateBasicTextFile(projDirStr, 'versions', 'rst',
+                          'v0.1.0 Generated on '
+                          + datetime.datetime.now().__str__() + '\n')
 
-def init_git_repo(rootdir: str, projectname: str, remotePath: str):
-    subprocess.run('cd ' + rootdir)
-    subprocess.run('git init ' + projectname)
+    initial_commit(projDirStr, remotePath)
+    generate_branches(projDirStr)
+
+
+def init_git_repo(rootDir: str, projectname: str):
+    print('Attempting to create git repo at \"' + rootDir + "\" called \"" +
+          projectname)
+    os.system('cd ' + rootDir)
+    os.system('git init ' + projectname)
+
 
 def generateBasicTextFile(dirstr: str, name: str, extension: str, initaltext:
         str):
@@ -27,8 +38,15 @@ def generateBasicTextFile(dirstr: str, name: str, extension: str, initaltext:
     file.write('########\n' + name.upper() + '\n########\n\n' + initaltext)
     file.close()
 
-def finish(rootDir: str, remotePath: str):
-    subprocess.run('cd ' + str)
-    subprocess.run('git add .')
-    subprocess.run("git commit -m \'initial commit, autogen.\'")
-    subprocess.run("git push --set-upstream ")
+
+def initial_commit(projdir: str, remotePath: str):
+    os.system('cd ' + projdir)
+    os.system('git add .')
+    os.system("git commit -m \'initial commit, autogen.\'")
+    os.system("git remote add origin " + remotePath)
+    os.system("git push -u origin master")
+
+
+def generate_branches(rootdir: str):
+    os.system('cd ' + rootdir)
+    os.system('git branch dev')
